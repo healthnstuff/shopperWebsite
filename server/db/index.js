cd8129706a7d4dcd9b7207858648be47aa508d87
Fernanda Penfold <fpenfold723@gmail.com>
2021-06-17 13:54:06 -0400

    fix(models): updates schema

--- server/db/index.js
+++ server/db/index.js
@@ -2,18 +2,13 @@
 
 const db = require("./db");
 
-<<<<<<< HEAD
-const User = require('./models/User')
-
-//associations could go here!
-=======
 const User = require("./models/static/user");
 const Address = require("./models/static/userAddress");
 const UserPayment = require("./models/static/userPayment");
 const Product = require("./models/static/product");
 const Category = require("./models/static/category");
-const CartItem = require("./models/CartItem");
-const Session = require("./models/Session");
+const CartItem = require("./models/processed/CartItem");
+const OrderInfo = require("./models/processed/orderInfo");
 
 //associations could go here!
 User.hasMany(Address);
@@ -25,43 +20,25 @@ UserPayment.belongsTo(User);
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
-Session.belongsTo(User);
-User.hasOne(Session);
-
-CartItem.belongsTo(Session);
-Session.hasMany(CartItem);
->>>>>>> 65d52167691d804ab1ef2de7132fa465b1779339
-
 /*
 Magic Methods
 https://medium.com/@julianne.marik/sequelize-associations-magic-methods-c72008db91c9
 */
 
-//processed model associations
-// OrderItem.belongsTo(Product)
-// Product.hasOne(OrderItem)
+CartItem.belongsTo(OrderInfo)
+OrderInfo.hasMany(CartItem)
 
-OrderItem.belongsTo(OrderInfo)
-OrderInfo.hasMany(OrderItem)
-
-// OrderInfo.belongsTo(User)
-// User.hasMany(OrderInfo)
+OrderInfo.belongsToMany(Product, { through: CartItem })
 
 module.exports = {
   db,
   models: {
-    // Product,
     OrderInfo,
-    OrderItem,
+    CartItem,
     User,
-<<<<<<< HEAD
-=======
     Address,
     UserPayment,
     Product,
     Category,
-    CartItem,
-    Session,
->>>>>>> 65d52167691d804ab1ef2de7132fa465b1779339
   },
 };
