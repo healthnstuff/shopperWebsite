43a025d7d869bc64e99b1ad690a2b535fd4d35fa
Hadeer Yousef <hadeeryousef10@gmail.com>
2021-06-21 17:32:03 -0400

    added action type, action creator and reducer for singleProduct

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,9 @@
+                                {/* <p>Decription: {product.description}</p>
+                                <span>Quantity: </span> 
+                                    <select>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>    */}
---
+++ client/store/singleProduct.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+//action types
+const SET_PRODUCT = 'SET_PRODUCT'
+
+//action creators
+export const setProduct = (product) => {
+  return {
+    type: SET_PRODUCT,
+    product
+  }
+};
+
+//thunk creators
+export const fetchSingleProduct = (id) => async (dispatch) => {
+  try {
+    const {data} = await axios.get(`/api/products/${id}`)
+      dispatch(setProduct(data))
+  } catch (error) {
+    console.log('There is an error:', error)
+  }
+};
+
+//initial state
+const initialState = {}
+
+//campusesreducer
+export default function singleProductReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCT:
+      return action.product
+    default:
+      return state
+  }
+}
+
+
+                                    
