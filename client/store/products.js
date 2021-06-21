6adc5e7da8e0a6c43eab042245e0f111b321ae17
Hadeer Yousef <hadeeryousef10@gmail.com>
2021-06-21 16:43:01 -0400

    chore: added action type, action creator and reducer for all products

---
+++ client/store/products.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+//action types
+const SET_PRODUCTS = 'SET_PRODUCTS'
+
+//action creators
+export const setProducts = (products) => {
+  return {
+    type: SET_PRODUCTS,
+    products
+  }
+};
+
+//thunk creators
+export const fetchProducts = () => async (dispatch) => {
+  try {
+    const {data} = await axios.get('/api/products')
+      dispatch(setProducts(data))
+  } catch (error) {
+    console.log('There is an error:', error)
+  }
+};
+
+//initial state
+const initialState = []
+
+//campusesreducer
+export default function productsReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products
+    default:
+      return state
+  }
+}
