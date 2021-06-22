555cbde674f0d4144874b7f76545366247b6b413
Elvy Yang <hyang6@wellesley.edu>
2021-06-23 01:21:52 +0800

    initiated localStorage

--- client/App.js
+++ client/App.js
@@ -4,16 +4,36 @@ import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import NavigationBar from "./components/NavigationBar";
 
-const App = () => {
-  return (
-    <div>
-      {/* <Navbar /> */}
-      {/* <Routes /> */}
-      <NavigationBar />
-      <Navbar />
-      <HomePage />
-    </div>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    this.localAdapter.saveCart([]);
+  }
+
+  localAdapter = {
+    saveCart: function (array) {
+      var stringified = JSON.stringify(array);
+      localStorage.setItem("cart", stringified);
+      return true;
+    },
+    getCart: function () {
+      return JSON.parse(localStorage.getItem("cart"));
+    },
+    clearCart: function () {
+      localStorage.removeItem("cart");
+    },
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <Navbar /> */}
+        {/* <Routes /> */}
+        <NavigationBar />
+        <Navbar />
+        <HomePage props={this.localAdapter} />
+      </div>
+    );
+  }
+}
 
 export default App;
