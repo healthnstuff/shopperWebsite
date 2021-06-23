a7b12fddb4cdfb5c91d267dad5933d75894c0c83
McKenna Warren <mckennakayyy@gmail.com>
2021-06-23 15:23:36 -0400

    fix

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -33,11 +33,11 @@ const NavigationBar = ({ user }) => {
             All Products
           </button>
         </NavLink>
-        <button className="categoryButton">Essential Oils</button>
-        <button className="categoryButton">Supplements</button>
-        <button className="categoryButton">Vitamins</button>
-        <button className="categoryButton">Tonic</button>
-        <button className="categoryButton">Tea</button>
+        <button className="categoryButton" type="button">Essential Oils</button>
+        <button className="categoryButton" type="button">Supplements</button>
+        <button className="categoryButton" type="button">Vitamins</button>
+        <button className="categoryButton" type="button">Tonic</button>
+        <button className="categoryButton" type="button">Tea</button>
       </div>
       <div>
         <Switch>
