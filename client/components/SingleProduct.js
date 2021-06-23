f1e44449a083746a3486ada425b1c4aac36543ac
Elvy Yang <hyang6@wellesley.edu>
2021-06-23 19:24:46 +0800

    style: singleProduct

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,44 +1,53 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchSingleProduct } from '../store/singleProduct'
+import { fetchSingleProduct } from "../store/singleProduct";
 
 class SingleProduct extends React.Component {
-    componentDidMount() {
-        const productId = this.props.match.params.productId
-        this.props.getSingleProduct(productId)
-    }
-    render () {
-        const product = this.props.singleProduct
-        return ( 
-            <div key={product.id}>
-                <img src={product.imageUrl} width="200" height="200"/>
-                    <p>Name: {product.name}</p>
-                    <p>Price: {product.price}</p>
-                    <p>Description: {product.description}</p>
-                    <span>Quantity: </span> 
-                    <select>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                    <button>ADD TO CART</button>
-            </div>
-        )
-    }
+  componentDidMount() {
+    const productId = this.props.match.params.productId;
+    this.props.getSingleProduct(productId);
+  }
+  render() {
+    const product = this.props.singleProduct;
+    return (
+      <div key={product.id} className="productContainer">
+        <img
+          src={product.imageUrl}
+          width="200"
+          height="200"
+          className="productImg"
+        />
+        <div className="productDescription">
+          <p style={{ fontSize: "40px", fontWeight: "bold" }}>
+            Name: {product.name}
+          </p>
+          <p>Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+          <span>Quantity: </span>
+          <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <button>ADD TO CART</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-      singleProduct: state.singleProduct
-    };
+  return {
+    singleProduct: state.singleProduct,
   };
-  
-const mapDispatch = (dispatch) => {
-    return {
-      getSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
-    }
 };
-  
+
+const mapDispatch = (dispatch) => {
+  return {
+    getSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
+  };
+};
+
 export default connect(mapState, mapDispatch)(SingleProduct);
